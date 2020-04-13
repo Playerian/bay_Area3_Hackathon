1356ff4801c6a4c0eeb852c9fc08fe170cf331b9
Glitch (create-react-app-sample) <none>
2020-04-13 21:49:48 +0000

    🗼🦄 Checkpoint ./package.json:789214/2759 ./public/index.html:789214/1301 ./src/index.js:789214/342

--- src/App.js
+++ src/App.js
@@ -2,20 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Sample from "./components/sample.js"
+import Map from "./components/map.js"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React(is cancer)</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save.
-        </p>
-        <h1>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</h1>
-        <Sample/>
+        
       </div>
     );
   }

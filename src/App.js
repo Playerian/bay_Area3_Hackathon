6408626597ae3a1d7e16b4949ef1f1f9ae8e5d4d
Glitch (create-react-app-sample) <none>
2020-04-13 21:04:01 +0000

    ⛩🌚 Checkpoint ./src/App.js:789214/62

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Sample from "./components/sample.js"
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save.
         </p>
         <h1>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</h1>
+        <Sample/>
       </div>
     );
   }

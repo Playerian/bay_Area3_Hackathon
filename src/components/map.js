5f33bfbb9758e439918bf4f8480a8110508c1012
Glitch (create-react-app-sample) <none>
2020-04-13 22:01:48 +0000

    🎁🎌 Checkpoint ./public/index.html:789040/730:789214/184 ./src/components/map.js:789214/383 ./src/index.js:789214/134 ./src/App.js:789040/57:789214/15

--- src/components/map.js
+++ src/components/map.js
@@ -3,9 +3,7 @@ import React, { Component } from 'react';
 export default class Map extends Component {
   render() {
     return (
-      <div className="sample">
-        Sample
-      </div>
+      <Map></Map>
     );
   }
 }

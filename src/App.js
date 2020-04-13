0286215bf45424e1963a8bb65a90aa2125b0493e
Glitch (create-react-app-sample) <none>
2020-04-13 23:02:38 +0000

    🙏🚆 Checkpoint ./src/components/map.js:789214/377 ./src/App.js:789214/48 ./src/components/Map.js:789214/83

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Sample from "./components/sample.js"
-import Map from "./components/map.js"
+import Map from "./components/Map.js"
 
 class App extends Component {
   render() {
--- src/components/Map.js
+++ src/components/Map.js
@@ -3,10 +3,18 @@ import React, { Component } from "react";
 const { Map: LeafletMap, TileLayer, Marker, Popup } = window.ReactLeaflet;
 
 export default class Map extends Component {
+  constructor(props){
+    super(props);
+  }
+  handleZoomEnd(e){
+    console.log(this.map);
+    console.log("X");
+  }
+  
   render() {
     return (
       // center of the US in coordinates: 40.2, -95.7129 (this is not the coordinates of the geographical center)
-      <LeafletMap center={[40.2, -95.7129]} zoom={5}>
+      <LeafletMap center={[40.2, -95.7129]} zoom={5} ref={(ref) => { this.map = ref; }} onZoomEnd={(e) => this.handleZoomEnd(e)}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'

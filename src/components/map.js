3d470bc455a8f45b1acf55f49abca117fb70d97c
Glitch (create-react-app-sample) <none>
2020-04-13 22:39:37 +0000

    🐮🏠 Checkpoint ./src/components/map.js:789040/33 ./src/App.css:789214/1

--- src/components/map.js
+++ src/components/map.js
@@ -5,7 +5,7 @@ const { Map: LeafletMap, TileLayer, Marker, Popup } = window.ReactLeaflet;
 export default class Map extends Component {
   render() {
     return (
-      <LeafletMap center={[40.0, -95.7129]} zoom={5}>
+      <LeafletMap center={[40.2, -95.7129]} zoom={5}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'

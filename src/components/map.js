c4653342ed424c4ac353c73fdbf7f077a95a7e40
Glitch (create-react-app-sample) <none>
2020-04-13 22:12:48 +0000

    ⛴💈 Checkpoint ./src/components/map.js:789040/779:789214/366 ./src/App.css:789214/380 ./public/index.html:789214/86

--- src/components/map.js
+++ src/components/map.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "https://unpkg.com/leaflet@1.4.0/dist/leaflet.css";
+const { Map: LeafletMap, TileLayer, Marker, Popup } = window.ReactLeaflet;
 
 export default class Map extends Component {
   render() {
-    return (
-      <Map></Map>
-    );
+    var mymap = LeafletMap.map('mapid').setView([51.505, -0.09], 13);
+    //return <LeafletMap center={[51.505, -0.09]} zoom={13} />;
+    return mymap;
   }
 }

0409d836072dc26fc0309ce529dec21151b85dd6
Glitch (create-react-app-sample) <none>
2020-04-14 03:46:38 +0000

    🚧🎈 Checkpoint ./src/components/Map.js:789040/5274 ./src/App.css:789040/291 ./src/App.js:789040/949 ./src/App.test.js:789040/450

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Map from "./components/Map.js"
-import borderGeoJSON from "./2010USoutline.json"
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Map from "./components/Map.js";
+import borderGeoJSON from "./2010USoutline.json";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="mapContainer">
-          
-          <Map center={{x:40.2, y:-95.7129}} borderGeoJSON={borderGeoJSON}/>
-          
+          <Map
+            center={{ x: 40.2, y: -95.7129 }}
+            borderGeoJSON={borderGeoJSON}
+          />
         </div>
       </div>
     );
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,18 +1,24 @@
 import React, { Component } from "react";
 //import "https://unpkg.com/leaflet@1.4.0/dist/leaflet.css";
-const { Map: LeafletMap, TileLayer, Marker, Popup, GeoJSON} = window.ReactLeaflet;
+const {
+  Map: LeafletMap,
+  TileLayer,
+  Marker,
+  Popup,
+  GeoJSON
+} = window.ReactLeaflet;
 
 export default class Map extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     //this.map does not exist yet
     this.state = {
       //coord: [latitude, longtitude]
       coord: [this.props.center.x, this.props.center.y],
       zoom: 5
-    }
+    };
   }
-  componentDidMount(){
+  componentDidMount() {
     //this.map exist
     let map = this.map.leafletElement;
     console.log(map);
@@ -24,61 +30,74 @@ export default class Map extends Component {
     // map.zoomControl.disable();
     //disable pan
     // map.dragging.disable();
-    
+
     //let marker = this.marker([37.7749,-122.4194]).addTo(map)
   }
-  handleZoomEnd(e){
+  handleZoomEnd(e) {
     console.log("Zoom ended");
     //set zoom as state
     let map = this.map.leafletElement;
     let zoom = map.getZoom();
     console.log(zoom);
     //minimum zoom is 4
-    if (zoom < 4){
+    if (zoom < 4) {
       zoom = 4;
     }
-    this.setState({zoom: zoom});
+    this.setState({ zoom: zoom });
   }
-  handleMoveEnd(e){
+  handleMoveEnd(e) {
     console.log("Move ended");
     var coord = e.target.getCenter();
     var lat = coord.lat;
     var lng = coord.lng;
-    console.log("Center of the map at latitude: " + lat + " and longitude: " + lng);
+    console.log(
+      "Center of the map at latitude: " + lat + " and longitude: " + lng
+    );
     //longtitude can't be less than -120
-    if (lng < -120){
-      lng = -120
+    if (lng < -120) {
+      lng = -120;
     }
     //longtitude can't be greater than -65
-    if (lng > -65){
-      lng = -65
+    if (lng > -65) {
+      lng = -65;
     }
     //latitude can't be greater than 50
-    if (lat > 50){
-      lat = 50
+    if (lat > 50) {
+      lat = 50;
     }
     //latitude can't be less than 25
-    if (lat < 25){
-      lat = 25
+    if (lat < 25) {
+      lat = 25;
     }
-    this.setState({coord: [lat, lng]});
+    this.setState({ coord: [lat, lng] });
   }
-  handleClick(e){
+  handleClick(e) {
     console.log("Clicked");
     var coord = e.latlng;
     var lat = coord.lat;
     var lng = coord.lng;
-    console.log("You clicked the map at latitude: " + lat + " and longitude: " + lng);
+    console.log(
+      "You clicked the map at latitude: " + lat + " and longitude: " + lng
+    );
   }
   render() {
     return (
       // center of the US in coordinates: 40.2, -95.7129 (this is not the coordinates of the geographical center)
-      <LeafletMap center={this.state.coord} zoom={this.state.zoom} ref={(ref) => { this.map = ref; }} onZoomEnd={(e) => this.handleZoomEnd(e)} onMoveEnd={(e) => this.handleMoveEnd(e)} onClick={(e) => this.handleClick(e)}>
+      <LeafletMap
+        center={this.state.coord}
+        zoom={this.state.zoom}
+        ref={ref => {
+          this.map = ref;
+        }}
+        onZoomEnd={e => this.handleZoomEnd(e)}
+        onMoveEnd={e => this.handleMoveEnd(e)}
+        onClick={e => this.handleClick(e)}
+      >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
-        <GeoJSON data={this.props.borderGeoJSON}/>
+        <GeoJSON data={this.props.borderGeoJSON} />
       </LeafletMap>
     );
   }

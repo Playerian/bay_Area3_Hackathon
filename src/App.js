2b51b9357b0c4d03a90138bfd2a178e0b93faab0
Glitch (create-react-app-sample) <none>
2020-04-14 22:30:24 +0000

    🍵🍐 Checkpoint ./src/App.css:789085/39 ./src/App.js:789085/32

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ class App extends Component {
             stateGeoJSON={stateGeoJSON}
             populationJSON={populationJSON}
           />
-          <div id="test"></div>
         </div>
       </div>
     );

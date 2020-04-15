a83cffc556acbd7011be860d8d77432e1c630589
Glitch (create-react-app-sample) <none>
2020-04-15 02:32:31 +0000

    🎾🎣 Checkpoint ./src/components/NumContainer.js:789197/3

--- src/components/NumContainer.js
+++ src/components/NumContainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-export default class Container extends Component{
+export default class NumContainer extends Component{
   constructor(props){
     super(props)
     

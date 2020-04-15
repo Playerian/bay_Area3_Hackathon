28adf73c5bc2ad2cbd0f1ab1ed2bc98367a17912
Glitch (create-react-app-sample) <none>
2020-04-15 18:46:30 +0000

    🍓🌶 Checkpoint ./src/components/Map.js:789214/15

--- src/components/Map.js
+++ src/components/Map.js
@@ -122,21 +122,21 @@ export default class Map extends Component {
       }else if (infected >= population * 0.05){
         color = colors[6];
       }else if (infected >= population * 0.1){
-        color = colors[6];
-      }else if (infected >= population * 0.2){
         color = colors[7];
-      }else if (infected >= population * 0.3){
+      }else if (infected >= population * 0.2){
         color = colors[8];
-      }else if (infected >= population * 0.4){
+      }else if (infected >= population * 0.3){
         color = colors[9];
-      }else if (infected >= population * 0.5){
+      }else if (infected >= population * 0.4){
         color = colors[10];
-      }else if (infected >= population * 0.6){
+      }else if (infected >= population * 0.5){
         color = colors[11];
-      }else if (infected >= population * 0.7){
+      }else if (infected >= population * 0.6){
         color = colors[12];
-      }else if (infected >= population * 0.8){
+      }else if (infected >= population * 0.7){
         color = colors[13];
+      }else if (infected >= population * 0.8){
+        color = colors[14];
       }
       layer.setStyle({
         color: color

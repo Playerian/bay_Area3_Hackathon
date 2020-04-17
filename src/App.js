bb72f8ffeb14bae00c60b17a7d9aca090ef3ad01
Glitch (create-react-app-sample) <none>
2020-04-17 19:22:23 +0000

    ⛑👢 Checkpoint ./src/App.js:789214/61

--- src/App.js
+++ src/App.js
@@ -519,6 +519,7 @@ class App extends Component {
           onUpgrade = {(upgrade) => this.onUpgrade(upgrade)
                        }
           upgradeInfo = {this.state.upgrades}
+          researchCompleted = {this.state.researchCompleted}
         />{/*buttons and counters goes in here?*/}
         
         {/*<div className="pplPointContiner"></div>
